6433265a1a92adc5b14b6288e38e7b2ac7b85c29
Abe Dolinger <abe@buddy.com>
2019-10-09 07:36:26 -0700

    added controllers/routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,8 @@
 Rails.application.routes.draw do
-  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  root 'donations#active'
+  resources :vendors
+  resources :stores
+  resources :donations
+  resources :claims
+  resources :clients
 end
