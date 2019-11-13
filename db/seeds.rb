864da9c3a31d2920a31bbe07154502030a296e34
Sofia <jonssonsofia93@gmail.com>
2019-11-12 16:56:06 -0800

    Seeded one Vendor

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,8 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+Vendor.destroy_all
+
+puts "Seeding Vendors..."
+v1 = Vendor.create(organization_name: "QFC", admin_email: "admin@qfc.com" password: "plaintext")
