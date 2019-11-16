503f1a5056602feffdf694e4a6af84d3df0bff14
Abe Dolinger <abe@buddy.com>
2019-11-16 09:23:47 -0800

    got donor JWT working

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 Rails.application.routes.draw do
   root 'donations#active'
+  post 'donor_auth', to: 'donor_auth#create'
   resources :donors, except: [:delete]
   resources :donations, except: [:delete]
   resources :claims, except: [:delete]
