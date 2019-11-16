f13bac0f298666fbe3dd0d67a914f14f31c57197
Sofia <jonssonsofia93@gmail.com>
2019-11-15 16:53:56 -0800

    Created C.R.U. for back end controllers. Removed stores controller

--- app/controllers/donations_controller.rb
+++ app/controllers/donations_controller.rb
@@ -1,5 +1,6 @@
 class DonationsController < ApplicationController
 	def index
+		@donations = Donation.all
 	end
 
 	def active
@@ -14,14 +15,31 @@ class DonationsController < ApplicationController
 	end
 
 	def create
+		@donation = Donation.create(donation_params)
+		if @donation.valid?
+			@token = encode_token(donation_id: @donation.id)
+			render json: { donation: DonationSerializer.new(@donation), jwt: @donation }, status: :created_at
+		else
+			render json: { error: 'failed to create donation' }, status: :not_acceptable
+		end
 	end
 
 	def edit
 	end
 
 	def update
+		if @doantion.update(donation_params)
+			redirect_to donation_path(@donation)
+		else
+			flash[:errors] = @donation.errors.full_messages
+			redirect_to edit_donation_path
+		end
 	end
 
-	def destroy
+	private
+
+	def donations_params
+		params.requier(:donations).permit(:food_name, :measurement, :per_person, :total_servings, :start_time, :duration_minutes, :image_url, :canceled)
 	end
+
 end
