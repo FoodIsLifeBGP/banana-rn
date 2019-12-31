2b99329e93d2035c4bf850403df99bec615c8308
VivianVKJ <1067937107@qq.com>
2019-12-30 20:12:49 -0500

    add passowrd show add hide

--- src/screens/LoginScreen/LoginScreen.styles.ts
+++ src/screens/LoginScreen/LoginScreen.styles.ts
@@ -1,18 +1,25 @@
-import { StyleSheet } from 'react-native';
-import * as colors from '../../util/colors';
+import { StyleSheet } from "react-native";
+import * as colors from "../../util/colors";
+import { white } from "react-native-paper/lib/typescript/src/styles/colors";
 
 export default StyleSheet.create({
-	outerContainer: {
-		flex: 1,
+  outerContainer: {
+    flex: 1,
     backgroundColor: colors.BANANA_YELLOW,
-		paddingHorizontal: '15%',
-	},
-	input: {
-		height: 50,
-		marginBottom: 15,
-		backgroundColor: 'white',
-		borderRadius: 0,
-		borderTopLeftRadius: 0,
-		borderTopRightRadius: 0,
-	}
+    paddingHorizontal: "15%"
+  },
+  input: {
+    height: 50,
+    marginBottom: 15,
+    backgroundColor: "white",
+    borderRadius: 0,
+    borderTopLeftRadius: 0,
+    borderTopRightRadius: 0
+  },
+  passwordContainer: {
+    height: 50,
+    flexDirection: "row",
+    justifyContent: "flex-start",
+    backgroundColor: "white"
+  }
 });
