39c783593f4b06f9155cd9d4b929d34cd0432c2f
Ernest Bruno <erndobruno@gmail.com>
2020-03-14 15:44:19 -0700

    Add new styles / rules for Login screen component.

--- src/screens/LoginScreen/LoginScreen.styles.ts
+++ src/screens/LoginScreen/LoginScreen.styles.ts
@@ -4,8 +4,6 @@ import * as colors from '@util/colors';
 export default StyleSheet.create({
 	outerContainer: {
 		flex: 1,
-		backgroundColor: colors.BANANA_YELLOW,
-		paddingHorizontal: '15%',
 	},
 	input: {
 		height: 50,
@@ -30,5 +28,24 @@ export default StyleSheet.create({
 	hideIcon: {
 		fontSize: 26,
 		color: colors.NAVY_BLUE,
+	},
+	banner: {
+		backgroundColor: colors.BANANA_YELLOW,
+		height: '35%',
+		paddingHorizontal: '15%',
+		paddingTop: 110,
+	},
+	innerContainer: {
+		paddingHorizontal: '15%',
+	},
+	buttonContainer: {
+		flexDirection: 'row',
+		paddingHorizontal: '8%',
+		justifyContent: 'space-between',
+		alignItems: 'center',
+	},
+	link: {
+		color: colors.NAVY_BLUE,
+		textAlign: 'right',
 	},
 });
