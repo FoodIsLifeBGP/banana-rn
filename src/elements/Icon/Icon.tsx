062d35712841239bd5adb72ebc90591e0c5a0a6c
Jackson Baker Ryan <jackson.baker.ryan@gmail.com>
2020-03-26 13:19:23 -0700

    Convert Icon component to use SVG assets
    - Maintain the client-facing, component props
    - Move types and utility functions to their respective files
      - String manipulation to string utility
      - Icon types and utility functions to Icon tsx

--- src/elements/Icon/Icon.tsx
+++ src/elements/Icon/Icon.tsx
@@ -1,15 +1,82 @@
 import React from 'react';
-import { Text, StyleProp, TextStyle } from 'react-native';
-import fontMap from './fontMap';
+import {
+	Image,
+	StyleProp,
+	ImageStyle,
+	ImageRequireSource,
+} from 'react-native';
+import { camelToUpperSkewer } from '@util/strings';
 import styles from './Icon.styles';
 
 interface IconProps {
-	name: string;
-	style?: StyleProp<TextStyle>;
+	name: IconName | DeprecatedIconName;
+	style?: StyleProp<ImageStyle>;
 }
 
-export default ({ name, style }: IconProps) => (
-	<Text style={[ styles.icon, style ]}>
-		{fontMap[name]}
-	</Text>
-);
+export default ({ name, style }: IconProps) => {
+	/**
+	 *
+	 * @param iconName
+	 */
+	const getIconSource = (iconName: IconName | DeprecatedIconName): ImageRequireSource => {
+		const getIconPath = (validIconName: IconName) => `@assets/icons/ICON_${camelToUpperSkewer(validIconName)}.svg`;
+		const nameIsDeprecated = Object.keys(deprecatedIconMap).includes(iconName);
+
+		const validIconName = nameIsDeprecated ? (deprecatedIconMap[iconName] || '') : iconName;
+
+		// eslint-disable-next-line import/no-dynamic-require
+		return require(getIconPath(validIconName));
+	};
+
+	return (
+		<Image style={[ styles.icon, style ]} source={getIconSource(name)} />
+	);
+};
+
+
+const deprecatedIconMap: Record<DeprecatedIconName, IconName | null> = {
+	'chevron-down': 'expand',
+	'chevron-left': 'back',
+	image: 'camera',
+	lock: null, // TODO: test if null is processed correctly --> i.e. no SVG is rendered w/o error
+	unlock: null,
+};
+
+export type DeprecatedIconName =
+		| 'chevron-down'
+		| 'chevron-left'
+		| 'image'
+		| 'lock'
+		| 'unlock';
+
+export type IconName =
+		| 'arrowDown'
+		| 'arrowUp'
+		| 'back'
+		| 'bell'
+		| 'bellOn'
+		| 'bike'
+		| 'camera'
+		| 'claims'
+		| 'close'
+		| 'condense'
+		| 'distance'
+		| 'donations'
+		| 'dropdown'
+		| 'email'
+		| 'expand'
+		| 'eyeOff'
+		| 'eyeOn'
+		| 'facebook'
+		| 'help'
+		| 'logout'
+		| 'menu'
+		| 'menuOn'
+		| 'more'
+		| 'qrCode'
+		| 'settings'
+		| 'time'
+		| 'transit'
+		| 'user'
+		| 'walk'
+		| 'website';
--- src/util/strings.ts
+++ src/util/strings.ts
@@ -1,3 +1,9 @@
 export const SCAN_QR_CODE = 'Scan QR Code';
 export const MY_DONATIONS = 'My Donations';
 export const LOG_OUT = 'Log Out';
+
+/**
+	 * Converts camel case to upper skewer case.
+	 * @param text
+	 */
+export const camelToUpperSkewer = (text: string): string => text.replace(/([A-Z])/g, '-$1').toUpperCase();
