0ab2744da0de273de296adcc750f8af9cc9028bf
Jackson Baker Ryan <jackson.baker.ryan@gmail.com>
2020-03-25 17:33:35 -0700

    Add standardized typography styles
    h1-h6
    body1-body6

---
+++ src/util/typography.ts
@@ -0,0 +1,59 @@
+import * as colors from '@util/colors';
+
+export default {
+	h1: {
+		fontFamily: 'open-sans-bold',
+		fontSize: 40,
+		color: colors.NAVY_BLUE,
+	},
+	h2: {
+		fontFamily: 'open-sans-bold',
+		fontSize: 25,
+		color: colors.NAVY_BLUE,
+	},
+	h3: {
+		fontFamily: 'open-sans-bold',
+		fontSize: 20,
+		color: colors.NAVY_BLUE,
+	},
+	h4: {
+		fontFamily: 'open-sans-bold',
+		fontSize: 16,
+		color: colors.NAVY_BLUE,
+	},
+	h5: {
+		fontFamily: 'open-sans-bold',
+		fontSize: 12,
+		color: colors.NAVY_BLUE,
+	},
+	body1: {
+		fontFamily: 'open-sans-regular',
+		fontSize: 16,
+		color: colors.NAVY_BLUE,
+	},
+	body2: {
+		fontFamily: 'open-sans-regular',
+		fontSize: 16,
+		color: colors.LIGHT_BLUE,
+	},
+	body3: {
+		fontFamily: 'open-sans-regular',
+		fontSize: 14,
+		color: colors.NAVY_BLUE,
+	},
+	body4: {
+		fontFamily: 'open-sans-regular',
+		fontSize: 14,
+		color: colors.NAVY_BLUE,
+	},
+	body5: {
+		fontFamily: 'open-sans-regular',
+		fontSize: 12,
+		color: colors.NAVY_BLUE,
+	},
+	body6: {
+		fontFamily: 'open-sans-regular',
+		fontSize: 12,
+		color: colors.NAVY_BLUE,
+	},
+};
