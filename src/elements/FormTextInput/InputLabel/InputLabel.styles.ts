c338680718364b38d6bd17cb70a78c7ad426b7d9
Jackson Baker Ryan <jackson.baker.ryan@gmail.com>
2020-03-29 14:33:19 -0700

    Update input label
    - Apply typography styling
      - Remove unnecessary capitalize prop
    - Add proper margin
    - Remove restricting container height

--- src/elements/FormTextInput/InputLabel/InputLabel.styles.ts
+++ src/elements/FormTextInput/InputLabel/InputLabel.styles.ts
@@ -1,15 +1,11 @@
 import { StyleSheet } from 'react-native';
-import * as colors from '@util/colors';
+import typography from '@util/typography';
 
 export default StyleSheet.create({
 	container: {
-		height: 25,
+		marginBottom: 5,
 	},
-	text: {
-		fontFamily: 'open-sans-bold',
-		fontStyle: 'normal',
-		fontSize: 16,
-		lineHeight: 22,
-		color: colors.NAVY_BLUE,
+	label: {
+		...typography.h3,
 	},
 });
