ec825063fe97105a1e66516200a4a69b95863e9c
Jackson Baker Ryan <jackson.baker.ryan@gmail.com>
2020-03-29 16:12:40 -0700

    Update FormTextInput
    - Update styling to meet new style guide
    - Remove extraneous props
    - Change prop names to improve clarity
    - Make prop-transparent to pass on react text input props
    - Add error style and error message
    - Remove inline styling

--- src/elements/FormTextInput/FormTextInput.styles.ts
+++ src/elements/FormTextInput/FormTextInput.styles.ts
@@ -1,25 +1,36 @@
 import { StyleSheet } from 'react-native';
+import typography from '@util/typography';
 import * as colors from '@util/colors';
 
+const INPUT_PADDING_HORIZONTAL = 10;
+
 export default StyleSheet.create({
-	container: {
-	},
-	inlineContainer: {
-		flexDirection: 'row',
-		alignItems: 'center',
-		justifyContent: 'space-between',
-		marginBottom: 10,
-	},
 	input: {
-		height: 40,
-		marginBottom: 10,
-		backgroundColor: colors.LIGHT_YELLOW,
-		paddingHorizontal: 8,
-		paddingVertical: 10,
-		color: colors.NAVY_BLUE,
+		...typography.body1,
 		width: '100%',
+		height: 48,
+		marginBottom: 5,
+		paddingHorizontal: INPUT_PADDING_HORIZONTAL,
+		paddingVertical: 13,
+		backgroundColor: colors.LIGHT_YELLOW,
+		borderWidth: 2,
+		borderStyle: 'solid',
+		// Transparent border to prevent box-size adjustments when error border is revealed.
+		borderColor: 'transparent',
+	},
+	inputError: {
+		borderColor: colors.RED,
+	},
+	errorMessage: {
+		paddingHorizontal: INPUT_PADDING_HORIZONTAL,
+	},
+	errorMessageText: {
+		...typography.body5,
+		color: colors.RED,
 	},
 	disabled: {
+		// TODO: apply disabled color palette
 		backgroundColor: colors.LIGHT_GRAY_DISABLED,
+		color: colors.WHITE,
 	},
 });
--- src/elements/FormTextInput/FormTextInput.tsx
+++ src/elements/FormTextInput/FormTextInput.tsx
@@ -1,71 +1,60 @@
+/* eslint-disable react/jsx-props-no-spreading */
+
 import React from 'react';
 import {
 	View,
 	TextInput,
+	Text,
 	StyleProp,
 	TextStyle,
+	TextInputProps,
 } from 'react-native';
 import { InputLabel } from '@elements';
-import { Colors } from 'react-native-paper';
-import * as colors from '@util/colors';
+import { LIGHT_BLUE } from '@util/colors';
 import styles from './FormTextInput.styles';
 
-interface FormTextInputProps {
-	text: string;
-	placeholder?: string;
-	value: any;
-	setValue: any;
-	width?: number | string;
-	autoCapitalize?: 'words' | 'sentences' | 'none' | 'characters';
-	inline?: boolean;
-	upperCase?: boolean;
-	disabled?: boolean;
-	style?: StyleProp<TextStyle>;
+interface FormTextInputProps extends TextInputProps {
+	label: string; // Label for the input.
+	value: TextInputProps['value']; // Input value.
+	setValue: TextInputProps['onChangeText']; // Callback used with every keystroke within the input.
+	inputStyle?: StyleProp<TextStyle>; // Styling to override default input styling.
+	error?: boolean; // Whether or not there is an error associated with the given text.
+	errorMessage?: string; // User-facing message associated with an error.
 }
 
 export default ({
-	text,
-	placeholder,
+	label,
 	value,
 	setValue,
-	width = '100%',
-	autoCapitalize = 'none',
-	inline = false,
-	upperCase = true,
-	disabled = false,
-	style,
+	inputStyle,
+	error = false,
+	errorMessage,
+	editable = true,
+	...props
 }: FormTextInputProps) => (
-	inline
-		? (
-			<View style={styles.inlineContainer}>
-				<InputLabel text={text} upperCase={upperCase} />
-				<View style={{ width: 8 }} />
-				<View style={{ width, top: 5 }}>
-					<TextInput
-						value={value}
-						onChangeText={setValue}
-						style={[ styles.input, disabled && styles.disabled, style, { textAlign: 'right' } ]}
-						autoCapitalize={autoCapitalize}
-						editable={!disabled}
-						placeholder={placeholder}
-						placeholderTextColor={colors.FADED_NAVY_BLUE}
-					/>
-				</View>
-			</View>
-		)
-		: (
-			<View style={{ ...styles.container, width }}>
-				<InputLabel text={text} upperCase={upperCase} />
-				<TextInput
-					value={value}
-					onChangeText={setValue}
-					style={[ disabled && styles.disabled, styles.input, style ]}
-					autoCapitalize={autoCapitalize}
-					editable={!disabled}
-					placeholder={placeholder}
-					placeholderTextColor={colors.FADED_NAVY_BLUE}
-				/>
-			</View>
-		)
-);
+	<View>
+		<InputLabel text={label} />
 
+		<View>
+			<TextInput
+				value={value}
+				onChangeText={setValue}
+				style={[
+					styles.input,
+					inputStyle,
+					!editable && styles.disabled,
+					error && styles.inputError,
+				]}
+				editable={editable}
+				placeholderTextColor={LIGHT_BLUE}
+				{...props}
+			/>
+
+			{error && (
+				<View style={styles.errorMessage}>
+					<Text style={styles.errorMessageText}>{errorMessage}</Text>
+				</View>
+			)}
+		</View>
+	</View>
+);
