cc762505809c77c7fe7ea6c07d41999b8b514102
Jackson Baker Ryan <jackson.baker.ryan@gmail.com>
2020-03-31 19:07:55 -0700

    Update Contact screen to new Figma

--- src/screens/ContactScreen/ContactScreen.styles.ts
+++ src/screens/ContactScreen/ContactScreen.styles.ts
@@ -1,41 +1,65 @@
 import { StyleSheet } from 'react-native';
-import * as colors from '@util/colors';
+import * as Colors from '@util/colors';
+import typography from '@util/typography';
+
+const GRID_MARGIN = 20; // Grid One from Style Guide TODO: make grid constants global
+const LIST_ITEM_BORDER_COLOR = Colors.LIGHT_GRAY;
+
+export const ListItem = StyleSheet.create({
+	container: {
+		paddingHorizontal: GRID_MARGIN,
+		paddingVertical: 10,
+		borderWidth: 1,
+		borderStyle: 'solid',
+		borderColor: 'transparent',
+		borderBottomColor: LIST_ITEM_BORDER_COLOR,
+	},
+	firstContainer: {
+		borderTopColor: LIST_ITEM_BORDER_COLOR,
+	},
+	title: {
+		flexDirection: 'row',
+		alignItems: 'center',
+	},
+	titleText: {
+		...typography.h3,
+	},
+	titleIcon: {
+		marginRight: 12,
+	},
+	message: {
+		...typography.body1,
+	},
+});
 
 export default StyleSheet.create({
-	headingText: {
-		fontFamily: 'open-sans-bold',
-		fontSize: 20,
-		color: colors.NAVY_BLUE,
-		alignSelf: 'center',
-	},
-	documentText: {
-		fontFamily: 'open-sans-regular',
-		fontSize: 18,
-		color: colors.NAVY_BLUE,
-		alignSelf: 'center',
-		justifyContent: 'center',
-		textAlignVertical: 'center',
-		textAlign: 'center',
-	},
-	headerContainer: {
-		paddingHorizontal: '10%',
-	},
 	outerContainer: {
 		flex: 1,
-		backgroundColor: colors.BANANA_YELLOW,
 	},
-	contentContainer: {
+	header: {
+		flexBasis: '16%',
+		alignItems: 'center',
+		justifyContent: 'center',
+		backgroundColor: Colors.BANANA_YELLOW,
+	},
+	headerText: {
+		...typography.h3,
+	},
+	bodyContainer: {
 		flex: 1,
-		backgroundColor: 'white',
-		padding: 25,
 	},
-	contactMethodHeader: {
-		fontFamily: 'open-sans-bold',
-		fontSize: 20,
-		color: colors.NAVY_BLUE,
+	bodyMessage: {
+		paddingHorizontal: GRID_MARGIN, // TODO: update based on Figma Question #65
+		paddingVertical: GRID_MARGIN,
 	},
-	contactMethodText: {
-		color: colors.NAVY_BLUE,
-		fontSize: 18,
+	bodyMessageTitle: {
+		...typography.h3,
+		alignSelf: 'center',
+		padding: GRID_MARGIN,
+	},
+	bodyMessageBody: {
+		...typography.body1,
+		paddingBottom: GRID_MARGIN,
+		textAlign: 'center',
 	},
 });
--- src/screens/ContactScreen/ContactScreen.tsx
+++ src/screens/ContactScreen/ContactScreen.tsx
@@ -1,60 +1,95 @@
 import React from 'react';
-import { Text, View } from 'react-native';
-import { Header, SpacerInline, Title } from '@elements';
+import {
+	ScrollView,
+	Text,
+	TouchableHighlight,
+	View,
+} from 'react-native';
 import { Linking } from 'expo';
-import styles from './ContactScreen.styles';
+import {
+	Header,
+	SpacerInline,
+} from '@elements';
+import styles, { ListItem } from './ContactScreen.styles';
+
+// TODO: Update interface to use IconName type
+const contactList: Array<{
+	title: string;
+	message: string;
+	link: string;
+	iconName: string;
+}> = [
+	{
+		title: 'website',
+		message: 'www.bananaapp.org',
+		// TODO: update to website, when built
+		link: 'https://bgpnow.wixsite.com/here/banana-app',
+		iconName: 'website',
+	},
+	{
+		title: 'email',
+		message: 'info@bananapp.org',
+		// TODO: Supposedly this link works on real devices, not simulators. Verify.
+		link: 'mailto:info@bananapp.org',
+		iconName: 'email',
+	},
+	{
+		title: 'facebook',
+		message: 'Connect with us on Facebook!',
+		// TODO: Fix this navigation
+		link: 'fb://profile?id=democracylaborg',
+		iconName: 'website',
+	},
+];
 
 export default () => (
+	// TODO: add scrollview depending on how UI team wants specific elements to scroll (or not)
 	<View style={styles.outerContainer}>
-		<View style={styles.headerContainer}>
-			<Header showMenu={true} />
+		<Header showMenu={true} />
+		{/* TODO: remove spacer after header is updated */}
+		<SpacerInline height={1} />
+
+		<View style={styles.header}>
+			<Text style={styles.headerText}>Contact Us</Text>
 		</View>
-		<Title text="Learn More" />
-		<SpacerInline height={20} />
-		<View style={styles.contentContainer}>
-			<SpacerInline height={20} />
-			<Text style={styles.headingText}>
-				CONTACT US
-			</Text>
-			<SpacerInline height={20} />
-			<Text style={styles.documentText}>
-				Feel free to reach out to us with any questions or inquiries! We will get back to you in 1-2 business days.
-			</Text>
-			<SpacerInline height={40} />
-			<Text style={styles.contactMethodHeader}>
-				WEBSITE
-			</Text>
-			<Text style={styles.contactMethodText} onPress={openBrowser}>
-				www.bananaapp.org
-			</Text>
-			<SpacerInline height={40} />
-			<Text style={styles.contactMethodHeader}>
-				EMAIL
-			</Text>
-			<Text style={styles.contactMethodText} onPress={openMail}>
-				thebananaapp@banana.org
-			</Text>
-			<SpacerInline height={40} />
-			<Text style={styles.contactMethodHeader}>
-				FACEBOOK
-			</Text>
-			<Text style={styles.contactMethodText} onPress={openFacebook}>
-				Connect with us on Facebook!
-			</Text>
-		</View>
+
+		<ScrollView style={styles.bodyContainer}>
+			{/* Body Message */}
+			<View style={styles.bodyMessage}>
+				<Text style={styles.bodyMessageTitle}>
+					We are here to help.
+				</Text>
+
+				<Text style={styles.bodyMessageBody}>
+					{
+						'Feel free to reach out to us with any questions or inquiries!'
+						+ ' \nWe will get back to you in 1-2 business days.'
+					}
+				</Text>
+			</View>
+
+			{/* Contact List */}
+			<View>
+				{
+					contactList.map((contact, i) => (
+						<TouchableHighlight key={contact.title} onPress={() => Linking.openURL(contact.link)}>
+							<View style={[ ListItem.container, i === 0 && ListItem.firstContainer ]}>
+								<View style={ListItem.title}>
+									{/* <Icon style={ListItem.titleIcon} name={contact.iconName} size={24} /> */}
+
+									<Text style={ListItem.titleText}>
+										{contact.title}
+									</Text>
+								</View>
+
+								<Text style={ListItem.message}>
+									{contact.message}
+								</Text>
+							</View>
+						</TouchableHighlight>
+					))
+				}
+			</View>
+		</ScrollView>
 	</View>
 );
-
-const openBrowser = () => {
-	Linking.openURL('https://www.google.com/search?q=banana');
-};
-
-const openMail = () => {
-	// TODO: Supposedly this works on real devices, not simulators. Verify.
-	Linking.openURL('mailto:thebananaapp@banana.org');
-};
-
-const openFacebook = () => {
-	// TODO: Fix this navigation
-	Linking.openURL('fb://profile?id=democracylaborg');
-};
