d6fbdcb9c05235ce08d00965ea5f65aaad23f191
Rahul Rajaram <rahulrajaram2005@gmail.com>
2020-04-19 20:22:03 -0700

    Move DonationsOrClaims to library/
    Presently, significant code representing instantiable components,
    "Donation"s and "Claim"s, are defined inside "DashboardScreen/". These
    components may be required across screens such as donations-history
    or claims-history pages. While putting the code inside a shareable
    directory inside "screens/" is also an option, putting them inside a
    currently-non-existent "library/" directory which would represent
    concrete components is a better option. This follows the choice to host
    common page components inside "elements/" as opposed to putting them
    inside a shareable directory within "screens/".

--- src/screens/DashboardScreen/DashboardScreen.tsx
+++ src/screens/DashboardScreen/DashboardScreen.tsx
@@ -4,7 +4,7 @@ import { View, Text } from 'react-native';
 import { TouchableOpacity } from 'react-native-gesture-handler';
 import useGlobal from '@state';
 import { Title, SpacerInline, Header } from '@elements';
-import DonationsOrClaims from './DonationsOrClaims';
+import DonationsOrClaims from '../../library/DonationsOrClaims';
 import styles from './DashboardScreen.styles';
 
 const DashboardScreen = () => {
