d485a63c13eb4f647747ddc82424b9ca2b5ac680
duskcloudxu <duskcloudxu@gmail.com>
2020-04-22 12:55:54 -0700

    roll back unnecessary changes

--- src/state/actions/getLocation.ts
+++ src/state/actions/getLocation.ts
@@ -8,17 +8,8 @@ export const getLocation = async store => {
 		await store.setState({ coords });
 		return coords;
 	}
-	// switch to return a null position when device position API is available;
 	return {
-		latitude: 47.618249,
-		longitude: 122.3520729,
-	};
-
-	// if latitute and longtitute are set to null
-	// There would be a weird mistake that the browser could not get list of item even if
-	// the position check logic at backend is turned off
-	return {
-		latitude: 47.618249,
-		longitude: 122.3520729,
+		latitude: null,
+		longitude: null,
 	};
 };
