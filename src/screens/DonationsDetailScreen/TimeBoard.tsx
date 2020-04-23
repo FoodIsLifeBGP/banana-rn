df924a0972f39c2b5722139305801bf53123b773
duskcloudxu <duskcloudxu@gmail.com>
2020-04-23 13:57:18 -0700

    Fix the "Can't perform a React state update on an unmounted component" warning

--- src/screens/DonationsDetailScreen/TimeBoard.tsx
+++ src/screens/DonationsDetailScreen/TimeBoard.tsx
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React, { useEffect, useState } from 'react';
 import {
 	View, Text, StyleProp, TextStyle,
 } from 'react-native';
@@ -15,8 +15,13 @@ export default ({ deadline, style }: TimeBoardProps) => {
 	const calcRemainingSec = (futureTime: Date) => Math.floor(((futureTime.getTime() - Date.now()) % 60000) / 1000);
 	const [ min, setMin ] = useState(calcRemainingMin(deadline));
 	const [ sec, setSec ] = useState(calcRemainingSec(deadline));
-	setInterval(() => setMin(calcRemainingMin(deadline)), 1000);
-	setInterval(() => setSec(calcRemainingSec(deadline)), 1000);
+	useEffect(() => {
+		const intervalHandle = setInterval(() => {
+			setMin(calcRemainingMin(deadline));
+			setSec(calcRemainingSec(deadline));
+		}, 1000);
+		return () => clearInterval(intervalHandle);
+	});
 	return (
 		<View style={[ styles.outerContainer, style ]}>
 			<View style={styles.flexCenterContainer}>
