41bd8201ff943f91db0656c705c54d7eca8cb5e9
Ernest Bruno <ernest.bruno94@yahoo.com>
2020-04-27 11:44:32 -0700

    Add styles for modal contents.

--- src/screens/QRCodeScannerScreen/QRCodeScannerScreen.styles.ts
+++ src/screens/QRCodeScannerScreen/QRCodeScannerScreen.styles.ts
@@ -1,4 +1,5 @@
 import { StyleSheet } from 'react-native';
+import * as colors from '@util/colors';
 
 export default StyleSheet.create({
 	container: {
@@ -6,4 +7,38 @@ export default StyleSheet.create({
 		justifyContent: 'flex-end',
 		backgroundColor: 'white',
 	},
+	modal: {
+		flex: 1,
+	},
+	content:{
+		flex: 1,
+		justifyContent: 'center',
+		alignItems: 'center',
+		color: colors.NAVY_BLUE,
+	},
+	textContainer: {
+		marginVertical: 3,
+		color: colors.NAVY_BLUE,
+		justifyContent: 'center',
+		alignItems: 'center',
+	},
+	textStyle: {
+		color: colors.NAVY_BLUE,
+	},
+	buttonTextStyle: {
+		fontSize: 12,
+		paddingHorizontal: 15,
+	},
+	iconStyle: {
+		color: 'blue',
+		fontSize: 20,
+	},
+	circle: {
+		height: 75,
+		width: 75,
+		borderWidth: 2,
+		borderColor: 'blue',
+		borderRadius: 50,
+		marginBottom: 10,
+	}
 });
