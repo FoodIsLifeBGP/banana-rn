bea44701ea0feef0cdf8fde5e3adfb6bfb1b0331
Ernest Bruno <erndobruno@gmail.com>
2020-04-28 13:44:44 -0700

    Revamp Login Screen UI and form interaction (#40)
    Commit history squashed because it is logically mixed and too long.
    Please review Github PR for commit history of these changes.
    
    ---
    
    - Update the UI according to the Figma.
    
    - Add ref forwarding to FormTextInput in order to more easily manipulate focus
      - e.g Focus moves to password field after locally submitting the email input
    
    - Add password type to FormTextInput that includes native input attributes
    and an option to hide the text using a custom eye icon button
    
    Co-authored-by: Jackson Baker Ryan <jackson.baker.ryan@gmail.com>

--- src/elements/FormTextInput/FormTextInput.tsx
+++ src/elements/FormTextInput/FormTextInput.tsx
@@ -1,6 +1,11 @@
 /* eslint-disable react/jsx-props-no-spreading */
 
-import React from 'react';
+import React, {
+	forwardRef,
+	RefObject,
+	Ref,
+	useState,
+} from 'react';
 import {
 	View,
 	TextInput,
@@ -9,52 +14,156 @@ import {
 	TextStyle,
 	TextInputProps,
 } from 'react-native';
-import { InputLabel } from '@elements';
+import {
+	Icon,
+	InputLabel,
+} from '@elements';
 import { LIGHT_BLUE } from '@util/colors';
+import { TouchableWithoutFeedback } from 'react-native-gesture-handler';
 import styles from './FormTextInput.styles';
 
-interface FormTextInputProps extends TextInputProps {
-	label: string; // Label for the input.
-	value: TextInputProps['value']; // Input value.
-	setValue: TextInputProps['onChangeText']; // Callback used with every keystroke within the input.
-	inputStyle?: StyleProp<TextStyle>; // Styling to override default input styling.
-	error?: boolean; // Whether or not there is an error associated with the given text.
-	errorMessage?: string; // User-facing message associated with an error.
+interface BasicTextInputProps extends TextInputProps {
+	/** User-submitted value. */
+	value: TextInputProps['value'];
+
+	/** Callback used with every keystroke within the input. */
+	setValue: TextInputProps['onChangeText'];
+
+	/** Styling to override default input styling. */
+	inputStyle?: StyleProp<TextStyle>;
+
+	/** Reference to the text input for programmatic manipulation. */
+	forwardedRef?: Ref<TextInput>;
 }
 
-export default ({
-	label,
+interface FormTextInputProps extends BasicTextInputProps {
+	/** Type text input. */
+	type?: 'default' | 'password';
+
+	/** Label for the input. */
+	label: string;
+
+	/** Whether or not there is an error associated with the given input value. */
+	error?: boolean;
+
+	/** User-facing message associated with an error. */
+	errorMessage?: string;
+}
+
+/**
+ * Most basic text input that is used across application.
+ */
+const BasicTextInput = ({
+	style,
 	value,
 	setValue,
 	inputStyle,
-	error = false,
-	errorMessage,
+	forwardedRef,
 	editable = true,
 	...props
-}: FormTextInputProps) => (
-	<View>
-		<InputLabel text={label} />
+}: BasicTextInputProps) => (
+	<TextInput
+		ref={forwardedRef}
+		value={value}
+		onChangeText={setValue}
+		style={[
+			styles.input,
+			style,
+			inputStyle, // Prop passed to this component to ensure it can override default styling.
+			!editable && styles.disabled,
+		]}
+		editable={editable}
+		placeholderTextColor={LIGHT_BLUE}
+		{...props}
+	/>
+);
 
+/**
+ * Input for a password with input visibility that can be toggled.
+ * Properties are 'transparent' and all are passed to the BasicTextInput.
+ */
+const PasswordInput = (
+	props: BasicTextInputProps,
+) => {
+	const [ isPasswordVisible, setIsPasswordVisible ] = useState(false);
+
+	return (
 		<View>
-			<TextInput
-				value={value}
-				onChangeText={setValue}
-				style={[
-					styles.input,
-					inputStyle,
-					!editable && styles.disabled,
-					error && styles.inputError,
-				]}
-				editable={editable}
-				placeholderTextColor={LIGHT_BLUE}
+			<BasicTextInput
 				{...props}
+
+				secureTextEntry={!isPasswordVisible}
+				autoCorrect={false}
+				textContentType="password"
 			/>
 
-			{error && (
-				<View style={styles.errorMessage}>
-					<Text style={styles.errorMessageText}>{errorMessage}</Text>
-				</View>
-			)}
+			<View style={styles.passwordVisibilityIcon}>
+				<TouchableWithoutFeedback
+					onPress={() => setIsPasswordVisible(!isPasswordVisible)}
+				>
+					<View style={styles.passwordIconContainer}>
+						<Icon
+							name={isPasswordVisible
+								? 'eyeOff'
+								: 'eyeOn'}
+							size={24}
+						/>
+					</View>
+				</TouchableWithoutFeedback>
+			</View>
 		</View>
-	</View>
-);
+	);
+};
+
+/**
+ * Input component for a form that includes a standardized label and text input.
+ * Can render a field with an optional visible password if 'type' password is given.
+ */
+const FormTextInput = (
+	{
+		type = 'default',
+		label,
+		value,
+		setValue,
+		error = false,
+		errorMessage,
+		style,
+		inputStyle,
+		...props
+	}: FormTextInputProps,
+	ref: Ref<TextInput>,
+) => {
+	const Input = type === 'password'
+		? PasswordInput
+		: BasicTextInput;
+
+	return (
+		<View style={style}>
+			<InputLabel text={label} />
+
+			<View>
+				<Input
+					style={error && styles.inputError}
+					forwardedRef={ref}
+					value={value}
+					setValue={setValue}
+					inputStyle={inputStyle}
+					{...props}
+				/>
+
+				{error && (
+					<View style={styles.errorMessage}>
+						<Text style={styles.errorMessageText}>
+							{errorMessage}
+						</Text>
+					</View>
+				)}
+			</View>
+		</View>
+	);
+};
+
+/**
+ * Allows the Higher-Order-Component (FormTextInput) to pass references to the native TextInput.
+ */
+export default forwardRef<TextInput, FormTextInputProps & { ref?: RefObject<TextInput> }>(FormTextInput);
--- src/screens/LoginScreen/LoginScreen.tsx
+++ src/screens/LoginScreen/LoginScreen.tsx
@@ -1,16 +1,20 @@
-import React, { useState } from 'react';
+import React, { useState, RefObject, createRef } from 'react';
 import { useNavigation, useNavigationParam } from 'react-navigation-hooks';
-import { View, Alert, TextInput } from 'react-native';
-import { TouchableWithoutFeedback } from 'react-native-gesture-handler';
+import {
+	ScrollView,
+	View,
+	Alert,
+	Text,
+	TextInput,
+	KeyboardAvoidingView,
+} from 'react-native';
 import useGlobal from '@state';
 import {
 	Title,
 	LinkButton,
-	SpacerInline,
-	InputLabel,
-	Icon,
+	FormTextInput,
 } from '@elements';
-import { NAVY_BLUE } from '@util/colors';
+import { TouchableWithoutFeedback } from 'react-native-gesture-handler';
 import styles from './LoginScreen.styles';
 
 export default () => {
@@ -19,13 +23,15 @@ export default () => {
 	const { userIdentity } = state;
 	const { logIn } = actions;
 
-	const defaultEmail = 'client@client.com';
-	const defaultPassword = 'client';
+	const passwordInputRef: RefObject<TextInput> = createRef();
+
 	const [ email, setEmail ] = useState(useNavigationParam('email') ?? '');
 	const [ password, setPassword ] = useState(useNavigationParam('password') ?? '');
-	const [ hidePwd, setHidePwd ] = useState(true);
+	const [ isPasswordVisible, setIsPasswordVisible ] = useState(false);
 
 	const clearEmailAndPassword = () => { setEmail(''); setPassword(''); };
+
+	const handleEmailInputSubmit = () => passwordInputRef.current?.focus();
 
 	const handleLogin = async () => {
 		const statusCode = await logIn({ email, password });
@@ -42,50 +48,65 @@ export default () => {
 		}
 	};
 
+	const handleForgotPassword = () => { console.log('Handle forgot password.'); };
+
 	return (
-		<View style={styles.outerContainer}>
-			<SpacerInline height={140} />
-			<Title text={`I am a ${userIdentity}.`} />
-			<SpacerInline height={40} />
-			<InputLabel text="Email Address" />
-			<TextInput
-				value={email}
-				onChangeText={setEmail}
-				style={styles.input}
-				autoCapitalize="none"
-				autoCorrect={false}
-				autoFocus={true}
-				blurOnSubmit={false}
-			/>
-			<InputLabel text="Password" />
-			<View style={styles.passwordContainer}>
-				<View style={{ flex: 8 }}>
-					<TextInput
-						textContentType="password"
-						value={password}
-						secureTextEntry={hidePwd}
-						onChangeText={setPassword}
-						style={styles.input}
-						autoCapitalize="none"
-						autoCorrect={false}
-					/>
-				</View>
-				<View style={styles.iconContainer}>
-					<TouchableWithoutFeedback
-						onPress={() => setHidePwd(!hidePwd)}
-					>
-						<Icon
-							name={hidePwd ? 'lock' : 'unlock'}
-							color={NAVY_BLUE}
-							size={24}
-						/>
-					</TouchableWithoutFeedback>
-				</View>
+		<KeyboardAvoidingView style={styles.outerContainer} behavior="padding">
+			<View style={styles.header}>
+				{/* TODO: use ContentHeader component when available */}
+				<Title text={`banana \n${userIdentity}`} />
 			</View>
-			<SpacerInline height={40} />
-			<LinkButton text="Log In" onPress={() => handleLogin()} />
-			<SpacerInline height={10} />
-			<LinkButton text="Register" destination="RegistrationScreen" />
-		</View>
+
+			<ScrollView style={styles.bodyContainer} contentContainerStyle={styles.bodyContentContainer}>
+				<View
+					style={styles.form}
+				>
+					<FormTextInput
+						label="email"
+						placeholder="info@bananaapp.org"
+						value={email}
+						setValue={setEmail}
+						style={styles.inputEmail}
+						onSubmitEditing={handleEmailInputSubmit}
+						autoCorrect={false}
+						enablesReturnKeyAutomatically={true}
+						autoCompleteType="username"
+						textContentType="username"
+						keyboardType="email-address"
+						returnKeyType="next"
+						blurOnSubmit={true} // Necessary to prevent focus from 'flickering'
+					/>
+
+					<FormTextInput
+						label="password"
+						type="password"
+						value={password}
+						setValue={setPassword}
+						ref={passwordInputRef}
+						onSubmitEditing={handleLogin}
+						enablesReturnKeyAutomatically={true}
+						autoCompleteType="password"
+						returnKeyType="go"
+						blurOnSubmit={false}
+					/>
+
+					<View style={styles.forgotPassword}>
+						{/* View wrapper required to constrain clickable area of button */}
+						<TouchableWithoutFeedback
+							onPress={handleForgotPassword}
+						>
+							<Text style={styles.forgotPasswordText}>
+								Forgot Password?
+							</Text>
+						</TouchableWithoutFeedback>
+					</View>
+				</View>
+
+				<View style={styles.buttonContainer}>
+					<LinkButton text="Log In" onPress={handleLogin} />
+					<LinkButton text="Register" destination="RegistrationScreen" />
+				</View>
+			</ScrollView>
+		</KeyboardAvoidingView>
 	);
 };
