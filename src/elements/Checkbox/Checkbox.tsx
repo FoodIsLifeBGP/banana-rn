1269f2f3eef25f2c5999e9c0aa5cded685e03db1
github username <josephtrettevik@gmail.com>
2020-05-04 13:50:20 -0700

    create functional Checkbox component

---
+++ src/elements/Checkbox/Checkbox.tsx
@@ -0,0 +1,41 @@
+import React from 'react';
+import { TouchableWithoutFeedback, View } from 'react-native';
+import { CheckBox } from 'react-native-elements';
+
+interface CheckboxProps {
+	checked: boolean;
+	setCheck: Function;
+	size?: number; // default = 18
+	disabled?: boolean; // default = false
+}
+
+export default function Checkbox({
+	checked,
+	setCheck,
+	size = 18,
+	disabled = false,
+}: CheckboxProps) {
+	const isDisabled = () => {
+		if (disabled) {
+			return (
+				<CheckBox
+					Component={TouchableWithoutFeedback}
+					size={size}
+				/>
+			);
+		}
+		return (
+			<CheckBox
+				checked={checked}
+				onPress={setCheck}
+				size={size}
+			/>
+		);
+	};
+
+	return (
+		<View>
+			{isDisabled()}
+		</View>
+	);
+}
