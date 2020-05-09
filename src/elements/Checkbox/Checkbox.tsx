0b08606dbb30bb03408242454d5567b47f77f282
Jackson Baker Ryan <jackson.baker.ryan@gmail.com>
2020-05-09 09:27:10 -0700

    Add interface documentation

--- src/elements/Checkbox/Checkbox.tsx
+++ src/elements/Checkbox/Checkbox.tsx
@@ -4,10 +4,29 @@ import { Icon } from '@elements';
 import { NAVY_BLUE, LIGHT_GRAY_DISABLED } from '@util/colors';
 
 interface CheckboxProps {
+	/**
+	 * Whether or not the checkbox is checked.
+	 */
 	checked: boolean;
+
+	/**
+	 * Callback to set the checked variable.
+	 */
 	setChecked: Function;
-	size?: number; // default = 18
-	disabled?: boolean; // default = false
+
+	/**
+	 * The square dimensions of the checkbox.
+	 */
+	size?: number;
+
+	/**
+	 * Whether or not the checkbox is disabled/ interactive.
+	 */
+	disabled?: boolean;
+
+	/**
+	 * Callback for when a user presses on the checkbox.
+	 */
 	onPress?: Function;
 }
 
