bf75683510b97c8826f2d68871b0bbb47aaee61e
kara-o <44376276+kara-o@users.noreply.github.com>
2020-05-09 13:45:53 -0700

    infoscreen padding layout

--- src/screens/InfoScreen/InfoScreen.styles.ts
+++ src/screens/InfoScreen/InfoScreen.styles.ts
@@ -4,22 +4,10 @@ import * as colors from '@util/colors';
 export default StyleSheet.create({
 	outerContainer: {
 		flex: 1,
+		flexDirection: 'column',
 		justifyContent: 'space-between',
+		alignItems: 'center',
 		backgroundColor: colors.BANANA_YELLOW,
-		paddingHorizontal: '11%',
-	},
-	input: {
-		height: 50,
-		marginBottom: 15,
-	},
-	documentContainer: {
-		backgroundColor: 'white',
-		paddingHorizontal: '5%',
-	},
-	documentText: {
-		fontFamily: 'open-sans-regular',
-		color: colors.NAVY_BLUE,
-		fontSize: 13,
-		textAlign: 'justify',
+		padding: 40,
 	},
 });
