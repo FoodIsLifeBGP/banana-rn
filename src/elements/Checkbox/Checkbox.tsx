036c056b976edfe551007e64497a7a8ccaabc9f9
Jackson Baker Ryan <jackson.baker.ryan@gmail.com>
2020-05-09 09:05:40 -0700

    Rename setCheck callback prop

--- src/elements/Checkbox/Checkbox.tsx
+++ src/elements/Checkbox/Checkbox.tsx
@@ -5,21 +5,21 @@ import { NAVY_BLUE } from '@util/colors';
 
 interface CheckboxProps {
 	checked: boolean;
-	setCheck: Function;
+	setChecked: Function;
 	size?: number; // default = 18
 	disabled?: boolean; // default = false
 }
 
 export default function Checkbox({
 	checked,
-	setCheck,
+	setChecked,
 	size = 18,
 	disabled = false,
 }: CheckboxProps) {
 	return (
 		<TouchableOpacity
 			disabled={disabled}
-			onPress={() => { setCheck(!checked); }}
+			onPress={() => { setChecked(!checked); }}
 		>
 			<Icon
 				name={checked ? 'arrowDown' : 'arrowUp'}
