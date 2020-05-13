09a6b69a0cab6531e8c0d13f3eb8667c597be8a7
hsiuyao2 <hsiuyao2@illinois.edu>
2020-05-13 16:58:47 -0500

    fix styling bug in ios simulator

--- src/elements/LinkButton/LinkButton.styles.ts
+++ src/elements/LinkButton/LinkButton.styles.ts
@@ -10,14 +10,12 @@ export default StyleSheet.create({
 	textContainer: {
 		height: 30,
 		width: 'auto',
-		borderBottomColor: 'white',
 		borderBottomWidth: 3.5,
 	},
 	text: {
 		textAlign: 'center',
 		fontFamily: 'open-sans-bold',
 		fontSize: 20,
-		color: colors.NAVY_BLUE,
 	},
 	pressed: {
 		color: 'white',
