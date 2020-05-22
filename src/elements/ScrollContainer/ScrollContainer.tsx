b3a6295cf6cc3b569da4511fe9e166cd37ef806c
github username <josephtrettevik@gmail.com>
2020-05-22 12:38:56 -0700

    add ScrollContainer to index

--- src/elements/ScrollContainer/ScrollContainer.tsx
+++ src/elements/ScrollContainer/ScrollContainer.tsx
@@ -2,34 +2,32 @@ import React from 'react';
 import { ScrollView } from 'react-native';
 
 interface ScrollContainerProps {
-  onScrollToEnd: Function,
-  style: Object;
-  children: Object;
+	onScrollToEnd: Function;
+	style: any;
+	children: any;
 }
 
 export default function ScrollContainer({
-  onScrollToEnd,
-  style,
-  children,
+	onScrollToEnd,
+	style,
+	children,
 }: ScrollContainerProps) {
-  
-  const isCloseToBottom = ({layoutMeasurement, contentOffset, contentSize}) => {
-    const paddingToBottom = 20;
-    return layoutMeasurement.height + contentOffset.y >=
-      contentSize.height - paddingToBottom;
-  };
+	const isCloseToBottom = ({ layoutMeasurement, contentOffset, contentSize }) => {
+		const paddingToBottom = 20;
+		return layoutMeasurement.height + contentOffset.y >= contentSize.height - paddingToBottom;
+	};
 
-  return (
-    <ScrollView
-      onScroll={({nativeEvent}) => {
-        if (isCloseToBottom(nativeEvent)) {
-          onScrollToEnd();
-        }
-      }}
-      style={style}
-      scrollEventThrottle={400}
-    >
-      {children}
-    </ScrollView>
-  );
+	return (
+		<ScrollView
+			onScroll={({ nativeEvent }) => {
+				if (isCloseToBottom(nativeEvent)) {
+					onScrollToEnd();
+				}
+			}}
+			style={style}
+			scrollEventThrottle={400}
+		>
+			{children}
+		</ScrollView>
+	);
 }
