4b6e50699e06ea91f26a1f2db89f0870a7ecf8d1
Ernest Bruno <ernest.bruno94@yahoo.com>
2020-05-25 11:03:17 -0700

    Create React component that render MainMenu elements (Scan QR Code text and its icon, etc.)

---
+++ src/elements/MenuDrawer/MainMenu/MainMenu.styles.tsx
@@ -0,0 +1,27 @@
+import { StyleSheet } from 'react-native';
+import * as colors from '@util/colors';
+
+export default StyleSheet.create({
+	mainMenu: {
+		display: 'flex',
+		width: '100%',
+		paddingLeft: 5,
+		flexDirection: 'row',
+		alignItems: 'center',
+		justifyContent: 'space-between',
+		borderTopWidth: 1,
+		borderTopColor: colors.WHITE,
+		paddingTop: 20,
+		marginBottom: 15,
+		marginTop: 5,
+		letterSpacing: 0.5,
+	},
+	menuText: {
+		color: colors.WHITE,
+		textTransform: 'uppercase',
+		marginLeft: 'auto',
+		marginRight: 5,
+		fontWeight: 'bold',
+		fontSize: 20,
+	}
+});
---
+++ src/elements/MenuDrawer/MainMenu/MainMenu.tsx
@@ -0,0 +1,42 @@
+import React from 'react';
+import { Text, View } from 'react-native';
+
+import { Icon } from '@elements';
+import { IconName } from '@elements/Icon';
+import * as colors from '@util/colors';
+
+import styles from './MainMenu.styles';
+
+const MainMenu = props => {
+	let menuStyle = {};
+	if (props.text === 'Contact Us') {
+		menuStyle = {
+			...styles.mainMenu,
+			marginBottom: 50,
+		};
+	} else if (props.text === 'Log out') {
+		menuStyle = { ...styles.mainMenu, borderTopWidth: 0, marginTop: 30 };
+	} else {
+		menuStyle = styles.mainMenu;
+	}
+
+	const DrawerIcon = (name: IconName) => Icon({
+		name,
+		size: 24,
+		color: colors.WHITE,
+	});
+
+	return (
+		<View style={menuStyle}>
+			<View>
+				{DrawerIcon(props.icon)}
+			</View>
+			<View>
+				<Text style={ styles.menuText }>{props.text}</Text>
+			</View>
+		</View>
+	);
+};
+
+
+export default MainMenu;
