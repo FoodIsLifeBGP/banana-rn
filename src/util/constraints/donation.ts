803f9f1219c6ef3db4308042f24264a43e5b7f58
bastion6429 <emarti43@ucsc.edu>
2020-05-31 17:14:54 -0700

    added donation validation constraint

---
+++ src/util/constraints/donation.ts
@@ -0,0 +1,21 @@
+export default {
+	itemName: {
+		presence: {
+			allowEmpty: false,
+		},
+	},
+	category: {
+		presence: {
+			allowEmpty: false,
+		},
+	},
+	totalAmount: {
+		allowEmpty: false,
+	},
+	pickupAddress: {
+		allowEmpty: false,
+	},
+	pickupInstructions: {
+		allowEmpty: false,
+	},
+};
