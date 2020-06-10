f0884d5f600537cc8347727222758809bfa6b7dc
renburnett <renardburnett@gmail.com>
2020-06-10 12:56:38 -0700

    add ContactUs screen, comment out HelpScreen, handle linking bug

---
+++ src/elements/ContentHeader/ContentHeader.tsx
@@ -0,0 +1,43 @@
+
+import React from 'react';
+import {
+	Text,
+	View,
+	StyleProp,
+	ViewStyle,
+	TextStyle,
+} from 'react-native';
+import styles from './ContentHeader.styles';
+
+export type HeaderSize = 'small' | 'large';
+
+// TODO: add props that allow client to select, small(ContactScreen) vs large header(LoginScreen)
+interface ContentHeaderProps {
+	title: string;
+	style?: StyleProp<ViewStyle>;
+	textStyle?: StyleProp<TextStyle>;
+	headerSize?: HeaderSize;
+}
+
+export default ({
+	title,
+	style,
+	textStyle,
+	headerSize = 'small',
+}: ContentHeaderProps) => {
+	const sizedContainerStyle = headerSize === 'small'
+		? styles.smallHeaderContainer
+		: styles.largeHeaderContainer;
+
+	const sizedTextStyle = headerSize === 'small'
+		? styles.smallHeaderText
+		: styles.largeHeaderText;
+
+	return (
+		<View style={[styles.headerContainer, sizedContainerStyle, style]}>
+			<Text style={[sizedTextStyle, textStyle]}>
+				{title}
+			</Text>
+		</View>
+	);
+};
