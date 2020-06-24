fc3c9744b3c92ec74650d3639f167466cb36e57d
justib41 <46506710+bethelju@users.noreply.github.com>
2020-06-23 21:41:05 -0700

    Added eslint-jest plugin

--- jest.config.js
+++ jest.config.js
@@ -1,9 +1,9 @@
 module.exports = {
-    projects: [
-      { preset: 'jest-expo/ios' },
-      { preset: 'jest-expo/android' }
-    ],
-    transformIgnorePatterns: [
-      'node_modules/(?!(jest-)?react-native|react-native-base64|react-clone-referenced-element|@react-native-community|expo(nent)?|@expo(nent)?/.*|react-utils|@react-utils/.*|@unimodules/.*|sentry-expo|native-base)'
-    ],
-  };
+	projects: [
+		{ preset: 'jest-expo/ios' },
+		{ preset: 'jest-expo/android' },
+	],
+	transformIgnorePatterns: [
+		'node_modules/(?!(jest-)?react-native|react-native-base64|react-clone-referenced-element|@react-native-community|expo(nent)?|@expo(nent)?/.*|react-utils|@react-utils/.*|@unimodules/.*|sentry-expo|native-base)',
+	],
+};
