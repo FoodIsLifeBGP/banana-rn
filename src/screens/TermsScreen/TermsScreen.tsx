5c06bdc101b57abab8bedf2360e75f0a29abbf94
hsiuyao2 <hsiuyao2@illinois.edu>
2020-06-24 20:39:53 -0500

    init refactor termscreen

--- src/screens/TermsScreen/TermsScreen.tsx
+++ src/screens/TermsScreen/TermsScreen.tsx
@@ -1,18 +1,37 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { View } from 'react-native';
-import { Title, SpacerInline, NavBar } from '@elements';
+import {
+	Title,
+	SpacerInline,
+	NavBar,
+	LinkButton,
+} from '@elements';
 import Terms from '@assets/documents/SampleToS';
 import ScrollContainer from '../../elements/ScrollContainer/ScrollContainer';
 import styles from './TermsScreen.styles';
 
-export default () => (
-	<View style={styles.outerContainer}>
-		<NavBar showMenu={false} />
-		<Title text="Terms and conditions" />
-		<SpacerInline height={20} />
-		<ScrollContainer
-			documentText={Terms}
-		/>
-		<SpacerInline height={80} />
-	</View>
-);
+export default () => {
+	const [button, setButton] = useState(true);
+	return (
+		<>
+			<View style={styles.titleContainer}>
+				<View style={styles.title}>
+					<Title text="Terms & conditions" />
+				</View>
+			</View>
+			<View style={styles.outerContainer}>
+				<SpacerInline height={20} />
+				<ScrollContainer
+					documentText={Terms}
+					onScrollToEnd={() => { setButton(false); }}
+				/>
+				<SpacerInline height={40} />
+				<LinkButton
+					text="back"
+					disabled={button}
+					destination="RegistrationScreen"
+				/>
+			</View>
+		</>
+	);
+};
