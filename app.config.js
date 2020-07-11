685ef859c4545eaa11e39cb29a2e2466619cb3d9
Drew Hite <drew@drewhite.org>
2020-07-10 23:17:08 -0400

    should be exportable to expo's website now and let you specify backend ip and app variant in a .env file

--- app.config.js
+++ app.config.js
@@ -1,52 +1,8 @@
-export default {
-	name: 'The Banana App',
-	slug: 'banana-app',
-	privacy: 'public',
-	sdkVersion: '36.0.0',
-	platforms: [
-		'ios',
-		'android',
-		'web',
-	],
-	version: '1.0.0',
-	orientation: 'portrait',
-	icon: './assets/icon.png',
-	splash: {
-		image: './assets/splash.png',
-		resizeMode: 'contain',
-		backgroundColor: '#083A9B',
-	},
-	updates: {
-		fallbackToCacheTimeout: 0,
-	},
-	assetBundlePatterns: [
-		'**/*',
-	],
-	ios: {
-		supportsTablet: true,
-	},
+export default ({ config }) => ({
+	...config,
 	extra: {
-		variant: process.env.APP_VARIANT,
+		variant: process.env.APP_VARIANT ? process.env.APP_VARIANT : 'donor',
 		storybook: false,
 		ipAddress: process.env.IP_ADDRESS,
 	},
-	userInterfaceStyle: 'automatic',
-	packagerOpts: {
-		config: 'metro.config.js',
-		sourceExts: [
-			'expo.ts',
-			'expo.tsx',
-			'expo.js',
-			'expo.jsx',
-			'ts',
-			'tsx',
-			'js',
-			'jsx',
-			'json',
-			'wasm',
-			'svg',
-			'md',
-
-		],
-	},
-};
+});
