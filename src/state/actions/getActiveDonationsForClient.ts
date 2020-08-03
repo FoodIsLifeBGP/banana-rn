c06f0135049da5a7ed9650792e83f08466f2b4ce
Drew Hite <drew@drewhite.org>
2020-08-03 16:13:40 -0400

    don't bomb if we don't have the user's location

--- src/state/actions/getActiveDonationsForClient.ts
+++ src/state/actions/getActiveDonationsForClient.ts
@@ -2,6 +2,11 @@ import railsAxios from '@util/railsAxios';
 
 export const getActiveDonationsForClient = async store => {
 	const { jwt, user } = store.state;
+	if (!user.coords) {
+		return [];
+	}
+	// leaving this around for a bit as we start to work through location testing and issues
+	console.log(`Here's the user coords ${JSON.stringify(user.coords)}`);
 	const endpoint = `/donations/active?client_lat=${user.coords.latitude}&client_long=${user.coords.longitude}`;
 	try {
 		const response = await railsAxios(jwt).get(endpoint);
