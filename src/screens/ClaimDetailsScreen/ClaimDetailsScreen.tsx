1bf12926b6f2065defb318460c169f83e2875973
Joseph Trettevik <49014210+CMChemist@users.noreply.github.com>
2020-08-14 20:41:17 -0700

    Directions in external gps app (#160)
    * create method to handle directions button click
    
    * optimize openGPS method

--- src/screens/ClaimDetailsScreen/ClaimDetailsScreen.tsx
+++ src/screens/ClaimDetailsScreen/ClaimDetailsScreen.tsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import { useNavigationParam, useNavigation } from 'react-navigation-hooks';
 import {
-	Dimensions, ImageBackground, ScrollView, Text, View,
+	Dimensions, ImageBackground, ScrollView, Text, View, Platform, Linking,
 } from 'react-native';
 import { Icon, SpacerInline, TextButton } from '@elements';
 import QRCode from 'react-native-qrcode-svg';
@@ -23,10 +23,19 @@ export default () => {
 			foreground: colors.WHITE,
 		},
 	};
+
 	const screenHeight = Math.round(Dimensions.get('window').height);
 	const screenWidth = Math.round(Dimensions.get('window').width);
 
 	const address = `${donor.address_street} ${donor.address_city}, ${donor.address_state}, ${donor.address_zip}`;
+
+	const openGPS = () => {
+		const url = Platform.select({
+			ios: `maps:0,0?q=${donor.donor_name}@${donor.latitude},${donor.longitude}`,
+			android: `geo:0,0?q=${address}`,
+		});
+		Linking.openURL(url);
+	};
 	return (
 
 		<View style={claimStyles.outerContainer}>
@@ -73,7 +82,7 @@ export default () => {
 							<Text style={typography.body4}>{donation.pickup_instructions}</Text>
 						</View>
 						<View style={{ flexDirection: 'row', justifyContent: 'center' }}>
-							<TextButton text="Directions" buttonStyle={claimBtnStyle} />
+							<TextButton text="Directions" buttonStyle={claimBtnStyle} onPress={() => openGPS()} />
 						</View>
 					</View>
 					<View>
