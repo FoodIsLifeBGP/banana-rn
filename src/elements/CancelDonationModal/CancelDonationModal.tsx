b3d66f1d1efd1e602505c42bf25fb885b752fd4d
Deepali Biradar <dbiradar18@gmail.com>
2020-09-06 15:05:32 -0400

    cancel donation & donation published modals (#172)
    * cancel donation & donation published modals
    
    * fixed linting errors
    
    * fixed linting errors

--- src/elements/CancelDonationModal/CancelDonationModal.tsx
+++ src/elements/CancelDonationModal/CancelDonationModal.tsx
@@ -16,25 +16,31 @@ import styles from './CancelDonationModal.styles';
 interface CancelDonationModalProps {
 	onYes?: () => void;
 	onNo?: () => void;
+	okay?: () => void;
 }
 
 export default ({
 	onYes = () => {},
 	onNo = () => {},
+	okay = () => {},
+
 }: CancelDonationModalProps) => {
 	const [ globalState, globalActions ] = useGlobal() as any;
 	const { alert: alertObj }: { alert: Alert } = globalState;
 	const { clearAlert } = globalActions;
 	const scheme = useScheme();
+	const [ state, { updateAlert } ] = useGlobal() as any;
 
 	const handleNo = () => {
 		clearAlert();
 		onNo();
+		alertObj.cancelFn && alertObj.cancelFn();
 	};
 
 	const handleYes = () => {
 		clearAlert();
 		onYes();
+		alertObj.confirmFn && alertObj.confirmFn();
 	};
 
 	const handleCloseButtonPress = () => {
@@ -45,6 +51,12 @@ export default ({
 		if (alertObj.dismissable) {
 			clearAlert();
 		}
+	};
+
+	const handleOkay = () => {
+		clearAlert();
+		okay();
+		alertObj.confirmFn && alertObj.confirmFn();
 	};
 
 	if (!alertObj) return null;
@@ -92,10 +104,9 @@ export default ({
 				<View style={styles.body}>
 					<View style={styles.textContainer}>
 						<Text style={typography.body1}>
-							{alertObj?.message || 'This donation will be canceled.'}
+							{alertObj?.message || 'This donation will be cancelled.'}
 						</Text>
 					</View>
-
 					<View style={styles.buttonWrapper}>
 						<View style={styles.leftButton}>
 							<TextButton
@@ -123,6 +134,41 @@ export default ({
 									disabled: scheme.disabled,
 								}}
 								onPress={handleYes}
+							/>
+						</View>
+					</View>
+				</View>
+			</Modal>
+		);
+	}
+	if (alertObj.type === 'donation published') {
+		return (
+			<Modal
+				style={styles.container}
+				title={alertObj?.title || 'DONATION PUBLISHED'}
+				palette="secondary"
+				open={alertObj !== undefined}
+				onDismiss={handleDismiss}
+			>
+				<View style={styles.body}>
+					<View style={styles.textContainer}>
+						<Text style={typography.body1}>
+							{alertObj?.message || 'Donation was published successfully. Thanks for your participation.'}
+						</Text>
+					</View>
+					<View style={styles.buttonWrapper}>
+						<View style={styles.leftButton}>
+							<TextButton
+								text="OKAY"
+								style={{
+									width: 120,
+								}}
+								buttonStyle={{
+									default: scheme.tertiary,
+									pressed: scheme.secondary,
+									disabled: scheme.disabled,
+								}}
+								onPress={handleOkay}
 							/>
 						</View>
 					</View>
