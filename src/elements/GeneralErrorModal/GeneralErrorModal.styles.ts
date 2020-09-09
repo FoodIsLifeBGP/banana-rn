10e5e985c0a6f47c6f0bf8670d2662db1b0d94f5
Joseph Trettevik <49014210+CMChemist@users.noreply.github.com>
2020-09-09 17:00:35 -0500

    Common error modals (#174)
    * create GeneralError modal shell
    
    * style and add functionality to GeneralError modal
    
    * rename GeneralError component to GeneralErrorModal and move to elements
    
    * revert LoginScreen.tsx
    
    * revert Modal.styles.ts
    
    * fixed linting errors

---
+++ src/elements/GeneralErrorModal/GeneralErrorModal.styles.ts
@@ -0,0 +1,19 @@
+import { StyleSheet } from 'react-native';
+import typography from '@util/typography';
+
+export default StyleSheet.create({
+	container: {
+		display: 'flex',
+		flexDirection: 'column',
+		alignItems: 'center',
+		justifyContent: 'space-between',
+	},
+	text: {
+		...typography.body1,
+		textAlign: 'center',
+	},
+	button: {
+		width: 104,
+		height: 46,
+	},
+});
