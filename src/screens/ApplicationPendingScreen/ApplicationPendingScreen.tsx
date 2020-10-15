41a861d444b846499990c9ab8a8d73bf997bc65b
Drew Hite <drew@drewhite.org>
2020-10-15 17:45:02 -0400

    fixing android map and non-functioning learn more link

--- src/screens/ApplicationPendingScreen/ApplicationPendingScreen.tsx
+++ src/screens/ApplicationPendingScreen/ApplicationPendingScreen.tsx
@@ -6,13 +6,11 @@ import InfoScreen from '../InfoScreen';
 export default () => (
 	<InfoScreen
 		title="Your application is being reviewed."
-		nextScreenTitle="Learn More"
-		nextScreenDestination="HelpScreen"
 		showBackButton={false}
 	>
 		<Paragraph fontSize={20}>
-      Please allow 24-48 hours for your registration to be reviewed. We will
-      send you an email once the application has been processed.
+			Please allow 24-48 hours for your registration to be reviewed. We will
+			send you an email once the application has been processed.
 		</Paragraph>
 		<SpacerInline height={40} />
 		<Paragraph emphasized={true} fontSize={20}>THANK YOU!</Paragraph>
