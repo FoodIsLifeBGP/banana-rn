1b0f676276771def5cc3e13e987ae13f3d8fb7d1
Chris Tack <chris_tack@mac.com>
2023-06-18 11:25:59 -0700

    Update config

--- metro.config.js
+++ metro.config.js
@@ -1,16 +1,19 @@
 /**
  * Configuration file for the React Native bundler, Metro.
- */
+*/
 
-const { getDefaultConfig } = require('metro-config');
+// Learn more https://docs.expo.io/guides/customizing-metro
+const { getDefaultConfig } = require('expo/metro-config');
 
+// extra config is needed to enable `react-native-svg-transformer`
 module.exports = (async () => {
 	const {
 		resolver: { sourceExts, assetExts },
-	} = await getDefaultConfig();
+	} = await getDefaultConfig(__dirname);
 	return {
 		transformer: {
 			babelTransformerPath: require.resolve('react-native-svg-transformer'),
+			assetPlugins: [ 'expo-asset/tools/hashAssetFiles' ],
 		},
 		resolver: {
 			assetExts: assetExts.filter(ext => ext !== 'svg'),
