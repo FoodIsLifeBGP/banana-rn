e5e4a5c8e8fa9d3e8f571a24d8341c310339f531
brendobrendo <brendan.smith903@gmail.com>
2023-08-20 21:43:59 -0700

    custom resolver

--- app.config.js
+++ app.config.js
@@ -2,16 +2,6 @@ import donorConfig from './app.donor.json';
 
 export default ({ config }) => {
 	require('dotenv').config();
-	// return {
-	// 	...config,
-	// 	extra: {
-	// 		ipAddress: process.env.EXPO_IP_ADDRESS,
-	// 		variant: process.env.EXPO_APP_VARIANT ? process.env.EXPO_APP_VARIANT : 'donor',
-	// 		storybook: process.env.EXPO_STORYBOOK ? process.env.EXPO_STORYBOOK === 'true' : false,
-	// 		useDefaultLocation: process.env.EXPO_USE_DEFAULT_LOCATION ? process.env.EXPO_USE_DEFAULT_LOCATION === 'true' : false,
-	// 		productionBuild: false,
-	// 	},
-	// };
 
 	const dynamicConfig = {
 		extra: {
@@ -19,7 +9,7 @@ export default ({ config }) => {
 			variant: process.env.EXPO_APP_VARIANT ? process.env.EXPO_APP_VARIANT : 'donor',
 			storybook: process.env.EXPO_STORYBOOK ? process.env.EXPO_STORYBOOK === 'true' : false,
 			useDefaultLocation: process.env.EXPO_USE_DEFAULT_LOCATION ? process.env.EXPO_USE_DEFAULT_LOCATION === 'true' : false,
-			productionBuild: false,
+			productionBuild: true,
 		},
 	};
 
--- metro.config.js
+++ metro.config.js
@@ -1,7 +1,17 @@
+const path = require('path');
 const { getDefaultConfig, mergeConfig } = require('@react-native/metro-config');
 /**
  * Configuration file for the React Native bundler, Metro.
 */
+
+const customResolver = {
+	'@assets': path.resolve(__dirname, './assets'),
+	'@elements': path.resolve(__dirname, './src/elements'),
+	'@screens': path.resolve(__dirname, './src/screens'),
+	'@state': path.resolve(__dirname, './src/state'),
+	'@library': path.resolve(__dirname, './src/library'),
+	'@util': path.resolve(__dirname, './src/util'),
+};
 
 const getDefaultExpoConfig = async () => {
 	const { resolver: { sourceExts, assetExts } } = await getDefaultConfig(__dirname);
@@ -14,6 +24,7 @@ const getDefaultExpoConfig = async () => {
 		resolver: {
 			assetExts: assetExts.filter(ext => ext !== 'svg'),
 			sourceExts: [ ...sourceExts, 'svg' ],
+			extraNodeModules: customResolver,
 		},
 	};
 };
