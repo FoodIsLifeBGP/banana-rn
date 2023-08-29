471e850a286194540401cb99571bec6df557ba1c
brendobrendo <brendan.smith903@gmail.com>
2023-08-28 22:12:17 -0700

    eslint fix- line length

--- app.config.js
+++ app.config.js
@@ -8,7 +8,8 @@ export default ({ config }) => {
 			ipAddress: process.env.EXPO_IP_ADDRESS,
 			variant: process.env.EXPO_APP_VARIANT ? process.env.EXPO_APP_VARIANT : 'donor',
 			storybook: process.env.EXPO_STORYBOOK ? process.env.EXPO_STORYBOOK === 'true' : false,
-			useDefaultLocation: process.env.EXPO_USE_DEFAULT_LOCATION ? process.env.EXPO_USE_DEFAULT_LOCATION === 'true' : false,
+			useDefaultLocation: process.env.EXPO_USE_DEFAULT_LOCATION
+				? process.env.EXPO_USE_DEFAULT_LOCATION === 'true' : false,
 			productionBuild: true,
 		},
 	};
