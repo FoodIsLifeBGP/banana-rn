b1f9bfadef8fe400c6c53fe3617dbb20c34695a5
brendobrendo <brendan.smith903@gmail.com>
2023-09-05 07:56:12 -0700

    Change env var names for expo consistency

--- app.config.js
+++ app.config.js
@@ -7,7 +7,7 @@ export default ({ config }) => {
 	const dynamicConfig = {
 		extra: {
 			ipAddress: process.env.EXPO_IP_ADDRESS,
-			variant: process.env.EXPO_APP_VARIANT ? process.env.EXPO_APP_VARIANT : 'donor',
+			variant: process.env.EXPO_PUBLIC_APP_VARIANT ? process.env.EXPO_PUBLIC_APP_VARIANT : 'donor',
 			storybook: process.env.EXPO_STORYBOOK ? process.env.EXPO_STORYBOOK === 'true' : false,
 			useDefaultLocation: process.env.EXPO_USE_DEFAULT_LOCATION
 				? process.env.EXPO_USE_DEFAULT_LOCATION === 'true' : false,
