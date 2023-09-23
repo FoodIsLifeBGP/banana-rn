5b28c414fe2b081fc69fd6b9b2783722384416b0
brendobrendo <brendan.smith903@gmail.com>
2023-09-23 14:32:29 -0700

    updates for dynamic config, adding env variables

--- app.config.js
+++ app.config.js
@@ -1,31 +1,57 @@
 import donorConfig from './app.donor.json';
 import clientConfig from './app.client.json';
 
+
 export default ({ config }) => {
 	require('dotenv').config();
 
 	const dynamicConfig = {
 		extra: {
 			ipAddress: process.env.EXPO_IP_ADDRESS,
-			// variant: process.env.EXPO_PUBLIC_APP_VARIANT ? process.env.EXPO_PUBLIC_APP_VARIANT : 'donor',
-			variant: 'client',
+			variant: process.env.EXPO_PUBLIC_APP_VARIANT ? process.env.EXPO_PUBLIC_APP_VARIANT : 'donor',
 			storybook: process.env.EXPO_STORYBOOK ? process.env.EXPO_STORYBOOK === 'true' : false,
 			useDefaultLocation: process.env.EXPO_USE_DEFAULT_LOCATION
 				? process.env.EXPO_USE_DEFAULT_LOCATION === 'true' : false,
-			productionBuild: true,
+			productionBuild: process.env.ENVIRONMENT_MODE === 'production',
 		},
 	};
+
+	if (process.env.ENVIRONMENT_MODE === 'production') {
+		if (process.env.EXPO_PUBLIC_APP_VARIANT === 'client') {
+			dynamicConfig.extra.eas = { projectId: process.env.CLIENT_APP_ID };
+		} else {
+			dynamicConfig.extra.eas = { projectId: process.env.DONOR_APP_ID };
+		}
+	}
+
+	let configVariant;
+
+	if (process.env.EXPO_PUBLIC_APP_VARIANT === 'client') {
+		configVariant = clientConfig;
+	} else {
+		configVariant = donorConfig;
+	}
+
+	// TODO: Delete later. 
+	// console.log({
+	// 	...config,
+	// 	...configVariant.expo,
+	// 	...dynamicConfig,
+	// 	extra: {
+	// 		...config.extra,
+	// 		...configVariant.expo.extra,
+	// 		...dynamicConfig.extra,
+	// 	},
+	// });
 
 	// Merge teh configurations
 	return {
 		...config,
-		// ...donorConfig.expo,
-		...clientConfig.expo,
+		...configVariant.expo,
 		...dynamicConfig,
 		extra: {
 			...config.extra,
-			// ...donorConfig.expo.extra,
-			...clientConfig.expo.extra,
+			...configVariant.expo.extra,
 			...dynamicConfig.extra,
 		},
 	};
